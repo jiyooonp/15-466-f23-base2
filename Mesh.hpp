fcea88a91f5786e27e6e44cb6fd1fa934c099b1b
jiyooonp <112532636+jiyooonp@users.noreply.github.com>
2023-09-11 17:25:00 -0400

    added some comments for understanding + playing around with the code

--- Mesh.hpp
+++ Mesh.hpp
@@ -15,43 +15,45 @@
 #include <limits>
 #include <string>
 
+struct Mesh
+{
+	// Meshes are vertex ranges (and primitive types) in their MeshBuffer:
 
-struct Mesh {
-	//Meshes are vertex ranges (and primitive types) in their MeshBuffer:
+	GLenum type = GL_TRIANGLES; // type of primitives in mesh
+	GLuint start = 0;			// index of first vertex
+	GLuint count = 0;			// count of vertices
 
-	GLenum type = GL_TRIANGLES; //type of primitives in mesh
-	GLuint start = 0; //index of first vertex
-	GLuint count = 0; //count of vertices
-
-	//Bounding box.
-	//useful for debug visualization and (perhaps, eventually) collision detection:
-	glm::vec3 min = glm::vec3( std::numeric_limits< float >::infinity());
-	glm::vec3 max = glm::vec3(-std::numeric_limits< float >::infinity());
+	// Bounding box.
+	// useful for debug visualization and (perhaps, eventually) collision detection:
+	glm::vec3 min = glm::vec3(std::numeric_limits<float>::infinity());
+	glm::vec3 max = glm::vec3(-std::numeric_limits<float>::infinity());
 };
 
-struct MeshBuffer {
-	//construct from a file:
-	// note: will throw if file fails to read.
+struct MeshBuffer
+{
+	// construct from a file:
+	//  note: will throw if file fails to read.
 	MeshBuffer(std::string const &filename);
 
-	//look up a particular mesh by name:
-	// note: will throw if mesh not found.
+	// look up a particular mesh by name:
+	//  note: will throw if mesh not found.
 	const Mesh &lookup(std::string const &name) const;
-	
-	//build a vertex array object that links this vbo to attributes to a program:
-	// note: will throw if program defines attributes not contained in this buffer
+
+	// build a vertex array object that links this vbo to attributes to a program:
+	//  note: will throw if program defines attributes not contained in this buffer
 	GLuint make_vao_for_program(GLuint program) const;
 
-	//This is the OpenGL vertex buffer object containing the mesh data:
+	// This is the OpenGL vertex buffer object containing the mesh data:
 	GLuint buffer = 0;
 
 	//-- internals ---
 
-	//used by the lookup() function:
-	std::map< std::string, Mesh > meshes;
+	// used by the lookup() function:
+	std::map<std::string, Mesh> meshes;
 
-	//These 'Attrib' structures describe the location of various attributes within the buffer (in exactly format wanted by glVertexAttribPointer). They are set when the file is loaded and are used by the "make_vao_for_program" call:
-	struct Attrib {
+	// These 'Attrib' structures describe the location of various attributes within the buffer (in exactly format wanted by glVertexAttribPointer). They are set when the file is loaded and are used by the "make_vao_for_program" call:
+	struct Attrib
+	{
 		GLint size = 0;
 		GLenum type = 0;
 		GLboolean normalized = GL_FALSE;
@@ -60,7 +62,7 @@ struct MeshBuffer {
 
 		Attrib() = default;
 		Attrib(GLint size_, GLenum type_, GLboolean normalized_, GLsizei stride_, GLsizei offset_)
-		: size(size_), type(type_), normalized(normalized_), stride(stride_), offset(offset_) { }
+			: size(size_), type(type_), normalized(normalized_), stride(stride_), offset(offset_) {}
 	};
 
 	Attrib Position;
--- PlayMode.hpp
+++ PlayMode.hpp
@@ -7,36 +7,44 @@
 #include <vector>
 #include <deque>
 
-struct PlayMode : Mode {
+struct PlayMode : Mode
+{
 	PlayMode();
 	virtual ~PlayMode();
 
-	//functions called by main loop:
+	// functions called by main loop:
 	virtual bool handle_event(SDL_Event const &, glm::uvec2 const &window_size) override;
 	virtual void update(float elapsed) override;
 	virtual void draw(glm::uvec2 const &drawable_size) override;
 
 	//----- game state -----
 
-	//input tracking:
-	struct Button {
+	// input tracking:
+	struct Button
+	{
 		uint8_t downs = 0;
 		uint8_t pressed = 0;
 	} left, right, down, up;
 
-	//local copy of the game scene (so code can change it during gameplay):
+	// local copy of the game scene (so code can change it during gameplay):
 	Scene scene;
 
-	//hexapod leg to wobble:
+	// hexapod leg to wobble:
 	Scene::Transform *hip = nullptr;
 	Scene::Transform *upper_leg = nullptr;
 	Scene::Transform *lower_leg = nullptr;
 	glm::quat hip_base_rotation;
 	glm::quat upper_leg_base_rotation;
 	glm::quat lower_leg_base_rotation;
-	float wobble = 0.0f;
-	
-	//camera:
-	Scene::Camera *camera = nullptr;
 
+	Scene::Transform *hip_ml = nullptr;
+	Scene::Transform *upper_leg_ml = nullptr;
+	Scene::Transform *lower_leg_ml = nullptr;
+	glm::quat hip_base_rotation_ml;
+	glm::quat upper_leg_base_rotation_ml;
+	glm::quat lower_leg_base_rotation_ml;
+	float wobble = 0.0f;
+
+	// camera:
+	Scene::Camera *camera = nullptr;
 };
